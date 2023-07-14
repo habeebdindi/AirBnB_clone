5371cd58c1d93d776f906fbf9565c9a05e50b6af
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-07-14 18:24:52 +0000

    Add first class and attributes

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+"""
+"""
+
+
+class BaseModel:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """
+        """
+        updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """
+        """
+        dict_kv = {}
+        dict_kv['__class__'] = self.__class__.__name__
+        for key, value in self.__dict__.items():
+            if key == "created_at":
+                dict_kv[key] = self.created_at.isoformat("T", "microseconds")
+            elif key == "updated_at":
+                dict_kv[key] = self.updated_at.isoformat("T", "microseconds")
+            else:
+                dict_kv[key] = value
+        return dict_kv
