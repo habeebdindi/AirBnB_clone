dde7dba770c2b85b145b4b938c94abf4d4e8b1da
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-07-15 15:32:10 +0000

    Resolve conflict

--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,16 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in  ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -23,7 +31,7 @@ class BaseModel:
     def save(self):
         """
         """
-        updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
