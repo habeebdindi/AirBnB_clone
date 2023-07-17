125c72fe0743cd974c71d1fb30a8738a0967ed59
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-07-17 03:17:52 +0000

    Import all dependencies

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,13 @@ and deserializes JSON file to instances.
 """
 import json
 import os.path    # Checking if a file exists
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 
 class FileStorage:
